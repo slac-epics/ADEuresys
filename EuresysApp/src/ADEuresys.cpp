47a92710529048ff2d96fa4c769af1806e261f9d
Bruce Hill <bhill@slac.stanford.edu>
2025-03-31 13:32:03 -0700

    Added support for setting NDBitsPerPixel
    Also tweaked XmitDelay calculation and ProcDelayPerPixel.

--- EuresysApp/src/ADEuresys.cpp
+++ EuresysApp/src/ADEuresys.cpp
@@ -250,6 +250,9 @@ void ADEuresys::processFrame(ScopedBuffer &buf)
     setIntegerParam(NDArraySizeY, (int)nRows);
     setIntegerParam(NDArraySize, (int)dataSize);
     setIntegerParam(NDDataType, dataType);
+#ifdef NDBitsPerPixelString
+	setIntegerParam(NDBitsPerPixel, bufferInfo.bitsPerPixel);
+#endif
     if (nDims == 3) {
         colorMode = NDColorModeRGB1;
     } 
@@ -295,7 +298,10 @@ void ADEuresys::processFrame(ScopedBuffer &buf)
         } else {
             pRaw->uniqueId = numImagesCounter;
         }
-            
+ 
+#ifdef NDBitsPerPixelString
+		pRaw->bitsPerElement = bufferInfo.bitsPerPixel;
+#endif
         updateTimeStamp(&pRaw->epicsTS);
         getIntegerParam(ESTimeStampMode, &timeStampMode);
         // Set the timestamps in the buffer
