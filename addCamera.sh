f0233a76c4ae2e460886007f7a0fc7f7a4002f9f
ktkim <ktkim@slac.stanford.edu>
2025-04-13 16:19:38 -0700

    Updated addCamera.sh for generating ADL UI files for the Euresys frame grabbers.

--- addCamera.sh
+++ addCamera.sh
@@ -9,9 +9,16 @@
 # Use this line for ao and ai records on older versions of EPICS base. 
 # Limits GenICam Integer features to 52 bits because ao and ai records with 64-bit floats are used.
 python3 scripts/makeDb.py xml/Euresys_Coaxlink_TLDataStream_25_02_0.xml EuresysApp/Db/Euresys_Coaxlink_TLDataStream_25_02_0.template --prefix DS
+python3 scripts/makeAdl.py xml/Euresys_Coaxlink_TLDataStream_25_02_0.xml EuresysApp/op/adl/Euresys_Coaxlink_TLDataStream_25_02_0 --prefix DS
+
 python3 scripts/makeDb.py xml/Euresys_Coaxlink_TLDevice_25_02_0.xml EuresysApp/Db/Euresys_Coaxlink_TLDevice_25_02_0.template --prefix DV
+python3 scripts/makeAdl.py xml/Euresys_Coaxlink_TLDevice_25_02_0.xml EuresysApp/op/adl/Euresys_Coaxlink_TLDevice_25_02_0 --prefix DV
+
 python3 scripts/makeDb.py xml/Euresys_Coaxlink_TLInterface_25_02_0.xml EuresysApp/Db/Euresys_Coaxlink_TLInterface_25_02_0.template --prefix IF
+python3 scripts/makeAdl.py xml/Euresys_Coaxlink_TLInterface_25_02_0.xml EuresysApp/op/adl/Euresys_Coaxlink_TLInterface_25_02_0 --prefix IF
+
 python3 scripts/makeDb.py xml/Euresys_Coaxlink_TLSystem_25_02_0.xml EuresysApp/Db/Euresys_Coaxlink_TLSystem_25_02_0.template --prefix SY
+python3 scripts/makeAdl.py xml/Euresys_Coaxlink_TLSystem_25_02_0.xml EuresysApp/op/adl/Euresys_Coaxlink_TLSystem_25_02_0 --prefix SY
 
 #python3 scripts/makeDb.py xml/$1.xml EuresysApp/Db/$1.template $2 $3
 #python3 scripts/makeAdl.py xml/$1.xml EuresysApp/op/adl/$1 $2 $3
